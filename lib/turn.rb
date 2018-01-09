23c602638cd2b050b392f7d3d21e7ea2403b3ca7
Tom Storm <tstorm@invoca.com>
2018-01-09 18:19:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,8 @@ def turn(board)
     index = input_to_index(user_input)
     if valid_move?(board, index) == true
       move(board, index, default_player = "X")
-    else turn(board)  
+    else turn(board) 
+    display_board(board)   
   end
 end
 
